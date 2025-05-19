77ae7db630996618dfff1084e64e7bb0cb8f544e
Shimanta Das <iamshimantadas@gmail.com>
2025-05-19 23:27:37 +0530

    first code

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+
+require_once('vendor/autoload.php');
+
+use NeuronAI\Agent;
+use NeuronAI\Chat\Messages\UserMessage;
+use NeuronAI\Providers\AIProviderInterface;
+use NeuronAI\Providers\Gemini\Gemini;
+
+class MyAgent extends Agent
+{
+    protected function provider(): AIProviderInterface
+    {
+        return new Gemini(
+            key: 'AIzaSyDVTsX1JUyxtExDmGfUOvdmVhyqEwwQVg4',
+            model: 'gemini-2.0-flash',
+        );
+    }
+}
+
+$response = MyAgent::make()->chat(new UserMessage("Hi!"));
+
+print_r($response);
+
+echo "<br/><br/>";
+
+var_dump($response);
